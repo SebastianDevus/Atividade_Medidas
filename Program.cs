632d5aa5464c59b9d19505605f727c305b4c8df1
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-19 13:39:56 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double metros, centimetros, quilometros;
+
+Console.Write("Por favor, insira a distância (em metros): ");
+metros = Convert.ToDouble(Console.ReadLine()!);
+
+centimetros = metros * 100;
+quilometros = metros / 1000;
+
+Console.WriteLine("-------Equivalência-------");
+Console.WriteLine($"\nCentímetros: {centimetros} cm\nMetros.....: {metros} m\nQuilômetros: {quilometros} Km");
